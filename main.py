65cf765425359c52a74a74de5264d15d26426f2b
Emile Cyimena <cyimena09@hotmail.com>
2022-05-22 08:31:46 +0200

    update for coders

--- main.py
+++ main.py
@@ -1,67 +1,49 @@
-import pygame as pygame
-import math
+import pygame
+
+# 1. générer la fenetre
 from game import Game
+from player import Player
 
-# 1. Chargement des éléments nécessaire pour faire fonctionner pygame
-pygame.init()
+pygame.display.set_caption("Coder dojo Shooter")  # titre de la fenetre
+screen = pygame.display.set_mode((1080, 720))  # taille de la fenetre
+background = pygame.image.load("assets/bg.jpg")  # fond d'écran de la fenetre
 
-# 2. Créer la fenetre du jeu (titre et taille, fond d'écran)
-pygame.display.set_caption("Coder dojo Shooter")
-screen = pygame.display.set_mode((1080, 720))
-background = pygame.image.load("assets/bg.jpg")
-
-# 3. Charger la bannière
-banner = pygame.image.load("assets/banner.png")
-banner = pygame.transform.scale(banner, (500, 500))  # redimensionner l'image
-banner_rect = banner.get_rect()  # positionner l'image
-banner_rect.x = math.ceil(screen.get_width() / 4)  # positionner en x
-
-# 4. Charger le bouton pour lancer la partie
-play_button = pygame.image.load("assets/button.png")
-play_button = pygame.transform.scale(play_button, (400, 150))
-play_button_rect = play_button.get_rect()  # positionner l'image
-play_button_rect.x = math.ceil(screen.get_width() / 3.33)  # positionner en x
-play_button_rect.y = math.ceil(screen.get_height() / 2)  # positionner en y
-
-# 5. Charger le jeu
+# générer le jeu
 game = Game()
+player = game.player
 
-# 6. On fait tourner le jeu indéfiniment. Chaque boucle correspond à un instant du jeu
+# 2. Le while True permet de garder la fenetre ouverte
 while True:
-    # 6.1. Appliquer l'arrière plan du jeu
     screen.blit(background, (0, -200))
+    game.update(screen)
 
-    # 6.2. Si le jeu a démarré, mettre à jour les éléments et les instructions du jeu
-    if game.is_playing:
-        game.update(screen)
-
-    # 6.3. Si le jeu n'a pas commencé, afficher l'écran de bienvenue
-    else:
-        screen.blit(play_button, play_button_rect)
-        screen.blit(banner, banner_rect)
-
-    # 6.4. Ecoute des évenements du clavier. Le joueur a avance, recule ? etc.
+    # Détecter les touches sur le clavier ou la souris
     for event in pygame.event.get():
-        # 6.4.1. Si le joueur décide de fermer le jeu
+
+        # 1.1. Si on a appuyé sur la croix
         if event.type == pygame.QUIT:
             pygame.quit()
+            print("Le joueur a quitté le jeu")
 
-        # 6.4.2. Détecter si le joueur appuie sur une touche du clavier
+        # 1.2. Si une touche du clavier a été appuyé
         elif event.type == pygame.KEYDOWN:
-            game.pressed[event.key] = True
+            # game.pressed[event.key] = True # faire une action sans devoir appuyer plusieurs fois sur la touche
+            print("Le joueur a appuyé sur une touche")
+            print(event.key)
 
-            # 6.4.2.1. Lancer un projectile si la barre espace est appuyée
-            if event.key == pygame.K_SPACE:
-                game.player.launch_projectile()
+            # Si le joueur a appuyé sur la touche A
+            if event.key == pygame.K_a:
+                print("On fait l'action A")
+                game.player.move_right()
+            elif event.key == pygame.K_b:
+                print("On fait l'action B")
 
-        # 6.4.3. Si le joueur à relacher une touche
+            # ....
+
+        # 1.3. Si une touche du clavier a été relaché
         elif event.type == pygame.KEYUP:
-            game.pressed[event.key] = False
+            # game.pressed[event.key] = True # permet d'arreter une action lorsque la touche est relachée
+            print("Le joueur a relaché une touche")
+            print(event.key)
 
-        # 6.4.4. Vérifier si on a cliqué sur le bouton
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # on récupère le rectangle du boutton et on vérifie si la souris est dessus
-            if play_button_rect.collidepoint(event.pos):
-                game.start()  # on lance le jeu
-
-    pygame.display.flip()  # mettre à jour l'affichage graphique
+    pygame.display.flip()
